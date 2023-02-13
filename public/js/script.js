ce4ad0986e979f8a0a17b27b812a311b3531f0f8
s223001489 <118330169+s223001489@users.noreply.github.com>
2023-02-13 16:21:44 +1100

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,17 +1,4 @@
-// const cardList = [
-//     {
-//         title: "Kitten 2",
-//         image: "images/kitten2.jpg",
-//         link: "About Kitten 2",
-//         desciption: "Demo desciption about kitten 2"
-//     },
-//     {
-//         title: "Kitten 3",
-//         image: "images/kitten2.jpg",
-//         link: "About Kitten 3",
-//         desciption: "Demo desciption about kitten 3"
-//     }
-// ]
+
 
 const getProjects = () => {
     $.get('/api/projects',(response) => {
@@ -19,46 +6,47 @@ const getProjects = () => {
             addCards(response.data);
         }
     })
-}
+  }
+
 
 const clickMe = () => {
     alert("Thanks for clicking me. Hope you have a nice day!")
 }
 
 const submitForm = () => {
-    let formData = {};
-    formData.title = $('#title').val();
-    formData.image = $('#image').val();
-    formData.link = $('#link').val();
-    formData.description = $('#description').val();
+  let formData = {};
+  formData.title = $('#title').val();
+  formData.image = $('#image').val();
+  formData.link = $('#link').val();
+  formData.description = $('#description').val();
 
-    console.log("Form Data Submitted: ", formData);
-    addProjectToApp(formData);
-    
+  console.log("Form Data Submitted: ", formData);
+  addProjectToApp(formData);
 }
 
 //ajax function...
 const addProjectToApp = (project) => {
-    $.ajax({
-        url: '/api/projects',
-        data: project,
-        type: 'POST',
-        success: (result) => {
-            alert(result.message);
-            location.reload(); // it automatically reloads the page 
-        }
-    })
+  $.ajax({
+      url: '/api/projects',
+      data: project,
+      type: 'POST',
+      success: (result) => {
+          alert(result.message);
+          location.reload(); // it automatically reloads the page ​
+
+      }
+  })
 }
 
 const addCards = (items) => {
     items.forEach(item => {
         let itemToAppend = '<div class="col s4 center-align">'+
-    '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+item.image+'">'+
+    '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="../images/'+item.image+'">'+
     '</div><div class="card-content">'+
     '<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right">more_vert</i></span><p><a href="#">'+item.link+'</a></p></div>'+
     '<div class="card-reveal">'+
         '<span class="card-title grey-text text-darken-4">'+item.title+'<i class="material-icons right">close</i></span>'+
-        '<p class="card-text">'+item.desciption+'</p>'+
+        '<p class="card-text">'+item.description+'</p>'+
       '</div></div></div>';
       $("#card-section").append(itemToAppend)
     });
@@ -72,3 +60,6 @@ $(document).ready(function(){
     getProjects();
     $('.modal').modal();
   });
+
+
+  
